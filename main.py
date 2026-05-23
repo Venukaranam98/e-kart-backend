5b2820da0506002a9a2a9d7a40b9cf85c8071fc7
venukaranam98 <vk3344556677@gmail.com>
2026-05-23 18:38:26 +0530

    Initial Ecommerce Backend

--- main.py
+++ main.py
@@ -4,26 +4,41 @@ from database import engine
 
 import models
 
-from routers import products,auth,cart,orders,payments
+from routers import (
+    products,
+    auth,
+    cart,
+    orders,
+    payments
+)
 
 from fastapi.staticfiles import StaticFiles
 
+import os
+
+
+os.makedirs(
+    "uploads",
+    exist_ok=True
+)
+
 
 app = FastAPI()
+
+
 app.mount(
-
     "/uploads",
-
     StaticFiles(directory="uploads"),
-
     name="uploads"
-
 )
+
+
 app.include_router(products.router)
 app.include_router(auth.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
 app.include_router(payments.router)
+
 
 models.Base.metadata.create_all(bind=engine)
 
