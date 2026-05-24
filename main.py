083efcc691c7e68843e728d710641d53527b512d
venukaranam98 <vk3344556677@gmail.com>
2026-05-24 14:14:02 +0530

    added cors

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ from fastapi.staticfiles import StaticFiles
 
 import os
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 os.makedirs(
     "uploads",
@@ -24,6 +26,26 @@ os.makedirs(
 
 
 app = FastAPI()
+
+origins = [
+
+    "http://localhost:5173",
+
+]
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=origins,
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
+)
 
 
 app.mount(
